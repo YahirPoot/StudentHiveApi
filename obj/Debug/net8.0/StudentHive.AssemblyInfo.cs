64e16f3af92b01c8a2a4e8c838f655de388d69a0
Gustavo Canul <gusssy12@gmail.com>
2024-02-01 17:48:07 -0600

    before to do changes

--- obj/Debug/net8.0/StudentHive.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentHive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentHive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e4203e3e83763e7f74e930ea84983b8ebc7d53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913071f9306403733621dd806ce84f2bb7e95d5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentHive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentHive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
