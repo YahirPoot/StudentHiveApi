dcc589fbba09824925126c7d3e5e9ee0dc8945ef
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-02-28 15:44:35 -0600

    add commit

--- obj/Debug/net8.0/StudentHive.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentHive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentHive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5656024d2e179ffb0645af2e8fad61629758c0a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507140616ca5c9d7f1f555879639ce5ae1118bcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentHive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentHive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
